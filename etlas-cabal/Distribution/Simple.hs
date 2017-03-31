30adad65898ae261df2aa2568e7ac124cf0321bc
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-03-31 10:56:27 +0530

    Updated Simple.hs

--- etlas-cabal/Distribution/Simple.hs
+++ etlas-cabal/Distribution/Simple.hs
@@ -566,8 +566,9 @@ simpleUserHooks =
        unregHook = \p l _ f -> unregister p l f
       }
   where
-    finalChecks _args flags pkg_descr lbi =
-      checkForeignDeps pkg_descr lbi (lessVerbose verbosity)
+    finalChecks _args flags pkg_descr lbi = return ()
+      -- TODO: Replace with a coursier check
+      -- checkForeignDeps pkg_descr lbi (lessVerbose verbosity)
       where
         verbosity = fromFlag (configVerbosity flags)
 

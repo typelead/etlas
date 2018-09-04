6fea18251b18e52a9dd7414bf732670ff82b68b4
jneira <atreyu.bbb@gmail.com>
2018-09-04 14:29:08 +0200

    Extract imports from cpp condition

--- etlas/Distribution/Client/SetupWrapper.hs
+++ etlas/Distribution/Client/SetupWrapper.hs
@@ -84,19 +84,19 @@ import qualified System.Process as Process
 import Distribution.Client.Compat.ExecutablePath  ( getExecutablePath )
 import System.IO
 import Distribution.Simple.PackageIndex
+
 #ifdef mingw32_HOST_OS
 import Distribution.Simple.Utils
          ( withTempDirectory )
-
 import Control.Exception   ( bracket )
-import System.FilePath     ( takeDirectory, (</>), (<.>) )
-import System.Directory    ( doesDirectoryExist, doesFileExist )
 import qualified System.Win32 as Win32
 #else
 import System.FilePath     ( (</>) )
 import System.Directory    ( doesFileExist )
 #endif
 
+import System.FilePath     ( takeDirectory, (</>), (<.>) )
+import System.Directory    ( doesDirectoryExist, doesFileExist )
 -- | @Setup@ encapsulates the outcome of configuring a setup method to build a
 -- particular package.
 data Setup = Setup { setupMethod :: SetupMethod
